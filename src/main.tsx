366349ae778d3e4692c97e59963abfe8bda8ab72
gustavohenri316 <gustavohenri316@gmail.com>
2022-07-04 17:10:46 -0300

    configurando MiraJs

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import {createServer} from 'miragejs'
 
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createaAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
