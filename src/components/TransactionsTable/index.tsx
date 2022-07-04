7b9eba5fd0b54c425a9890d74e1b5ee7bd900fdf
gustavohenri316 <gustavohenri316@gmail.com>
2022-07-04 16:44:17 -0300

    TransactionsTable

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,35 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$2.000</td>
+            <td>Casa</td>
+            <td>10/02/2021</td>
+          </tr>
+        </tbody>
+        
+      </table>
+    </Container>
+  )
+}
