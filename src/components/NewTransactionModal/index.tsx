728bb5ddbcdd00c863c55751ac635853fb320663
gustavohenri316 <gustavohenri316@gmail.com>
2022-07-11 15:40:43 -0300

    estilizando os botoes de entrada e saida

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,14 +1,18 @@
-import { Container, TransactionTypeContainer } from "./styles";
+import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 import Modal from 'react-modal'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
+import { useState } from "react";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void
 }
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [type, setType] = useState("deposit")
+
+
   return (
     <Modal
       isOpen={isOpen}
@@ -33,18 +37,24 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           placeholder="Valor"
         />
         <TransactionTypeContainer>
-          <button
+          <RadioBox
             type="button"
+            onClick={() => { setType('deposit')}}
+            isActive={type === 'deposit'}
+            activeColor='green'
           >
             <img src={incomeImg} alt="Entrada" />
             Entrada
-          </button>
-          <button
+          </RadioBox>
+          <RadioBox
             type="button"
+            onClick={() => { setType('withdraw')}}
+            isActive={type === 'withdraw'}
+            activeColor='red'
           >
             <img src={outcomeImg} alt="Saída" />
             Saída
-          </button>
+          </RadioBox>
         </TransactionTypeContainer>
         <input
           placeholder="Categoria"
