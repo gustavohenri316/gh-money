caf13b9b4b1701fefa3062b898ac1681c18a9bfb
gustavohenri316 <gustavohenri316@gmail.com>
2022-07-22 09:49:33 -0300

    formantando valores

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -12,11 +12,11 @@ interface Transaction {
   createdAt: string
 }
 export function TransactionsTable() {
-const [ transactions, setTransactions ] = useState<Transaction[]>([])
+  const [transactions, setTransactions] = useState<Transaction[]>([])
 
   useEffect(() => {
     api.get('/transactions')
-    .then(response => setTransactions(response.data.transactions))
+      .then(response => setTransactions(response.data.transactions))
   }, [])
 
   return (
@@ -31,18 +31,25 @@ const [ transactions, setTransactions ] = useState<Transaction[]>([])
           </tr>
         </thead>
         <tbody>
-         {transactions.map(transaction => {
-          return (
-            <tr key={transaction.id}>
-            <td>{transaction.title}</td>
-            <td className={transaction.type}>{transaction.amount}</td>
-            <td>{transaction.category}</td>
-            <td>{transaction.createdAt}</td>
-          </tr>
-          )
-         })}
+          {transactions.map(transaction => {
+            return (
+              <tr key={transaction.id}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>
+                  {
+                    new Intl.NumberFormat('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL'
+                    }).format(transaction.amount)
+                  }
+                </td>
+                <td>{transaction.category}</td>
+                <td>{new Intl.DateTimeFormat('pt-BR').format(new Date (transaction.createdAt))}</td>
+              </tr>
+            )
+          })}
         </tbody>
-        
+
       </table>
     </Container>
   )
